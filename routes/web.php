87f5b28abde6ac6614933d8c3b1340e6765bdb54
muhtarudinsiregar <6048482+muhtarudinsiregar@users.noreply.github.com>
2019-12-18 22:13:19 +0700

    e23 - Adding a Custom Middleware

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::view("about", "about");
 // Route::delete("customers/{customer}", "CustomerController@destroy");
 
 Route::resource("customers", "CustomerController");
-Route::get("contact ", "ContactFormController@create");
+Route::get("contact ", "ContactFormController@create")->middleware('test');
 Route::post("contact ", "ContactFormController@store");
 
 Auth::routes();
